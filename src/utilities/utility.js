b22a0551b4b03cce2ade7908b009d2bc624ab7ce
radsatu <56917790+radsatu@users.noreply.github.com>
2021-10-16 12:24:23 +0700

    Add initial config for random user api

--- src/utilities/utility.js
+++ src/utilities/utility.js
@@ -2,9 +2,21 @@ import {
     ApolloClient,
     InMemoryCache,
 } from "@apollo/client";
-import { API_POKEMON } from "./constants";
+import { 
+    API_POKEMON,
+    URL,
+} from "./constants";
 
 export const ApolloClinet = new ApolloClient({
     uri: API_POKEMON,
     cache: new InMemoryCache()
 });
+
+export const getUserData = () => {
+    const userData = fetch(URL.RANDOM_USER)
+                        .then(response => response.json())
+                        .then(data => data)
+                        .catch(error => console.log('user data', error));
+
+    return userData;
+}
